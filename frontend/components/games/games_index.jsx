b50a65cc076c8c2c82af5e8ca41ed789793f1551
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-03 17:58:07 -0700

    corrected alignment of content and fixed container tags for index items

--- frontend/components/games/games_index.jsx
+++ frontend/components/games/games_index.jsx
@@ -15,17 +15,16 @@ class GamesIndex extends React.Component {
         const game = this.props.games.map(game => <li><GameIndexItem key={game.id} game={game}/></li>)
 
         return (
-            <div className="games-directory">
-
+            <div className="game-index-item-container">
                 <div className="fake-ad-container">
-
                     <div className="fake-advertisement">
                         <p>This is going to be a fake ad</p>        
                     </div>
-                    
                 </div>
-                
-                <ul className="game-index-list-items">{game}</ul>
+
+                <div className="games-directory">
+                    <ul className="game-index-list-items">{game}</ul>
+                </div>
             </div>
         );
     }
