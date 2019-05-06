b87b2fdf660e3a31d315335026e12e5204c7bd01
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-06 12:21:51 -0700

    add video player and chat areas to channel pages

--- frontend/components/streamers/stream_index_item.jsx
+++ frontend/components/streamers/stream_index_item.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const StreamIndexItem = ({stream, streamer}) => (
     <div className="stream-index-item-information">
         <div className="game-video-thumbnail">
-            <p className="stream-username-tag"><Link to={`/channel/${streamer.id}`} style={{color: "rgb(100, 65, 165)", textDecoration: 'none' }}>{streamer.username}</Link></p>
-            <p className="stream-title-tag"><Link to={`/channel/${streamer.id}`} style={{color: "rgb(100, 65, 165)", textDecoration: 'none' }}>{stream.title}</Link></p>
+            <p className="stream-username-tag"><Link to={`/channel/${streamer.id}`} streamer={streamer} stream={stream} style={{color: "rgb(100, 65, 165)", textDecoration: 'none' }}>{streamer.username}</Link></p>
+            <p className="stream-title-tag"><Link to={`/channel/${streamer.id}`} streamer={streamer} stream={stream} style={{color: "rgb(100, 65, 165)", textDecoration: 'none' }}>{stream.title}</Link></p>
         </div>
     </div>
 );
--- frontend/components/streamers/streamer_channel.jsx
+++ frontend/components/streamers/streamer_channel.jsx
@@ -3,12 +3,40 @@ import { Route, Link } from 'react-router-dom';
 
 class StreamerChannel extends React.Component {
 
-    render() {
+    componentDidMount() {
+        
+    }
 
+    render() {
+        let {streamer} = this.props;
+        if(!streamer) {
+            return null;
+        }
         return (
-            <div className="channel-header-container">
-                <h1>This is only a test</h1>
-            </div>
+            <>
+                <div className="channel-header-container">
+                    <div className="user-information-top-bar">
+                        <ul>
+                            <li className="streamer-username">{streamer.username}</li>
+                        </ul>
+                    </div>
+                    <div className="chat-box">
+                        <div className="chat-box-top">
+                            <p>Stream Chat</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="video-player-container">
+                    <iframe
+                        src="https://clips.twitch.tv/embed?clip=IncredulousAbstemiousFennelImGlitch&autoplay=false"
+                        height="600"
+                        width="100%"
+                        frameborder="0"
+                        scrolling="no"
+                        allowfullscreen="true">
+                    </iframe>
+                </div>
+            </>
         );
 
     }
