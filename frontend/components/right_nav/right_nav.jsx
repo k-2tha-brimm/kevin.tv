6a4ecf916b58d82e630473ff718e09b5011e5e7b
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-06 22:45:17 -0700

    fix issue with AWS and heroku. add dropdown menu depending on state of log-in

--- frontend/components/right_nav/right_nav.jsx
+++ frontend/components/right_nav/right_nav.jsx
@@ -4,10 +4,24 @@ import React from 'react';
 // for now it is simply styled as text
 
 const RightNav = ({ currentUser, logout, openModal }) => {
+
     const sessionLinks = () => (
         <nav className="right-nav-signed-out">
             <button className="login-button" onClick={() => openModal('login')}>Log In</button>
             <button className="signup-button" onClick={() => openModal('signup')}>Sign Up</button>
+            <div className="dropdown-menu">
+                <button className="dropdown-trigger">Drop Me Down</button>
+                <div className="dropdown-items">
+                    <li>Online</li>
+                    <li>Channel</li>
+                    <div className="line-break"></div>
+                    <li>Settings</li>
+                    <li>Language</li>
+                    <li>Dark Theme</li>
+                    <div className="line-break"></div>
+                    <li>Log Out</li>
+                </div>
+            </div>
             {/* <i className="fas fa-otter"></i> */}
         </nav>
     );
@@ -16,6 +30,19 @@ const RightNav = ({ currentUser, logout, openModal }) => {
         <nav className="right-nav-signed-in"> 
             <li className="user-avatar">Welcome to Kevin.Tv, { currentUser.username }!</li>
             {/* <i className="fas fa-otter"></i> */}
+            <div className="dropdown-menu">
+                <button className="dropdown-trigger">Drop Me Down</button>
+                <div className="dropdown-items">
+                    <li>Online</li>
+                    <li>Channel</li>
+                    <div className="line-break"></div>
+                    <li>Settings</li>
+                    <li>Language</li>
+                    <li>Dark Theme</li>
+                    <div className="line-break"></div>
+                    <li>Log Out</li>
+                </div>
+            </div>
             <button className="nav-logout-button" onClick={logout}>Sign Out</button>
         </nav>
     );
--- frontend/components/user_dropdown/logged_in_dropdown_container.js
+++ frontend/components/user_dropdown/logged_in_dropdown_container.js
@@ -1,6 +1,17 @@
+import { connect } from 'react-redux';
+import DropDownMenu from './drop_down';
 
+const mapStateToProps = state => {
+    return {
+        menu: 'logged in'
+    };
+};
 
-
+const mapDispatchToProps = dispatch => {
+    return {
+        
+    };
+};
 
 
 
