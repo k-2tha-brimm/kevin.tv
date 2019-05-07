1db04d08c93286fa68016b4411c314f65cd27fd8
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-06 17:46:00 -0700

    add key to assist with production deployment utilizing AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.active_storage.service = :amazon_dev
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
