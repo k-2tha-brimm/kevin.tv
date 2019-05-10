6cb98efe39d360be77ff8b6d12a0ffe75ec7c5fa
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-10 07:31:43 -0700

    add functionality for pulling videos onto homepage

--- frontend/components/jumbotron/jumbotron.jsx
+++ frontend/components/jumbotron/jumbotron.jsx
@@ -1,21 +1,49 @@
 import React from 'react';
 
-const Jumbotron = props => (
-    <>
-        <div className="jumbotron">
-            <h1>I am a placeholder for something far greater</h1>
-        </div>
-        {/* <div>
-            <iframe
-                src="https://clips.twitch.tv/embed?clip=IncredulousAbstemiousFennelImGlitch&autoplay=false"
-                height="360"
-                width="640"
-                frameborder="0"
-                scrolling="no"
-                allowfullscreen="true">
-            </iframe>
-        </div> */}
-    </>
-);
+class Jumbotron extends React.Component {
+
+    render() {
+
+        // var httpRequest = new XMLHttpRequest();
+        // httpRequest.addEventListener('load', clipsLoaded);
+        // httpRequest.open('GET', 'https://api.twitch.tv/kraken/clips/top?limit=10&game=Overwatch&trending=true');
+        // httpRequest.setRequestHeader('Client-ID', 'spmus28uykcb190xqkv0abkh1uvwnc');
+        // httpRequest.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
+        // httpRequest.send();
+
+        // function clipsLoaded() {
+        //     var clipsDisplay = document.getElementById('featured-videos'),
+        //         clipList = JSON.parse(httpRequest.responseText);
+        
+        //     clipList.clips.forEach(function(clip, index, array) {
+        //         clipItem = document.createElement('div');
+        //         clipItem.innerHTML = clip.embed_html;
+        //         clipsDisplay.appendChild(clipItem);
+        //     });
+        // }
+
+
+        return (
+
+            <>
+                <div className="jumbotron">
+                    <iframe
+                        src="https://clips.twitch.tv/embed?clip=IncredulousAbstemiousFennelImGlitch&autoplay=false"
+                        height="600"
+                        width="86%"
+                        frameborder="0"
+                        scrolling="no"
+                        allowfullscreen="true">
+                    </iframe>
+                </div>
+
+                <div className="featured-videos"></div>
+                <div>
+    
+                </div>
+            </>
+        )
+    }
+}
 
 export default Jumbotron;
